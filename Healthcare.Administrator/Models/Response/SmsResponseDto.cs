c36e0d7d5c9bfe870ebff9f734ef3b4605a94128
Nalashaa IT <itsupport@nalashaa.com>
2018-11-22 19:30:13 +0530

    Response DTO commit

---
+++ Healthcare.Administrator/Models/Response/SmsResponseDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Healthcare.Administrator.Models.Response
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks></remarks>
+    public class SmsResponseDto
+    {
+        /// <summary>
+        /// Gets or sets .
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public string SmsID { get; set; }
+        /// <summary>
+        /// Gets or sets .
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public string SmsText { get; set; }
+        /// <summary>
+        /// Gets or sets .
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+     //   public string IsRead { get; set; }
+        /// <summary>
+        /// Gets or sets .
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public bool? IsSend { get; set; }
+        /// <summary>
+        /// Gets or sets .
+        /// </summary>
+        /// <value></value>
+        /// <remarks></remarks>
+        public DateTime Date { get; set; }     
+    }
+}
