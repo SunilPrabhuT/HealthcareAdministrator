713ecc8e1b9aa56e9531ba5e38694eab5b4d3916
Sunilp <Sunilp@SUNILP>
2018-11-25 12:46:00 +0530

    TokenChanges

--- Healthcare.Administrator/Auth/Providers/CustomJwtFormat.cs
+++ Healthcare.Administrator/Auth/Providers/CustomJwtFormat.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Web;
 using Thinktecture.IdentityModel.Tokens;
 using System.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Healthcare.Administrator.Providers
 {
@@ -55,8 +57,8 @@ namespace Healthcare.Administrator.Providers
 
             var expires = data.Properties.ExpiresUtc;
 
-            //var token = new JwtSecurityToken(_issuer, audienceId, data.Identity.Claims, issued.Value.UtcDateTime,
-            //    expires.Value.UtcDateTime, signingKey);
+            // var token = new JwtSecurityToken((_issuer, audienceId, data.Identity.Claims, issued.Value.UtcDateTime,
+            // expires.Value.UtcDateTime, signingKey);
 
             // var stringName = token.Payload.Where(i => i.Key == "nameid").Select(i => i.Value).FirstOrDefault();
 
@@ -67,7 +69,39 @@ namespace Healthcare.Administrator.Providers
 
             // Write the Jwt Token 
             //return jwt;
-            return null;
+
+            // Define const Key this should be private secret key  stored in some safe place
+           // string key = "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
+
+            // Create Security key  using private key above:
+            // not that latest version of JWT using Microsoft namespace instead of System
+            var securityKey = new Microsoft
+               .IdentityModel.Tokens.SymmetricSecurityKey(Encoding.UTF8.GetBytes(symmetricKeyAsBase64));
+
+            // Also note that securityKey length should be >256b
+            // so you have to make sure that your private key has a proper length
+            //
+            var credentials = new Microsoft.IdentityModel.Tokens.SigningCredentials
+                              (securityKey, SecurityAlgorithms.HmacSha256Signature);
+
+            //  Finally create a Token
+            var header = new JwtHeader(credentials);
+
+            //Some PayLoad that contain information about the  customer
+            var payload = new JwtPayload
+           {
+               { "some ", "hello "},
+               { "scope", "http://dummy.com/"},
+           };
+
+            //
+            var secToken = new JwtSecurityToken(header, payload);
+            var handler = new JwtSecurityTokenHandler();
+
+            // Token to String so you can use it in your client
+            var tokenString = handler.WriteToken(secToken);
+
+            return tokenString;
         }
         /// <summary>
         /// Unprotects the token
